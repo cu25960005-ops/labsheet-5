2b3b32e37476336ee7b7121efef349c3c6cedd82
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 20:32:10 +0530

    Perfect number

---
+++ 16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+    for (int i = 1; i < n; i++)
+        if (n % i == 0) sum += i;
+    if (sum == n)
+        cout << "Perfect number";
+    else
+        cout << "Not perfect";
+    return 0;
+}
