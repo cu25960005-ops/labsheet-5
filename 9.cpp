6dfa0e85cc09d76b6b3e3b2a71bca57c5c1145d7
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 20:27:35 +0530

    Demonstrate default switch

---
+++ 9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice = 9;
+    switch (choice) {
+        case 1: cout << "Case 1"; break;
+        case 2: cout << "Case 2"; break;
+        default: cout << "Default case executed";
+    }
+    return 0;
+}
