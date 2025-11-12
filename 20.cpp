0b75a46bd707cce1c11e51425d42619342c73dba
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 20:33:36 +0530

    Reverse & check palindrome

---
+++ 20.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, rev = 0, temp;
+    cout << "Enter number: ";
+    cin >> n;
+    temp = n;
+    while (temp != 0) {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+    cout << "Reverse = " << rev << endl;
+    if (rev == n) cout << "Palindrome";
+    else cout << "Not palindrome";
+    return 0;
+}
